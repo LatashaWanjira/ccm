4b4a8b1597b7d401eb5cc393e4d09abf03658b4d
LatashaWanjira <latasha.wanjira@gmail.com>
2019-05-27 17:50:58 +0300

    new design changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,16 @@ $(document).ready(function() {
     var $nav = $(".navbar");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+
+  // Dropdown Toggle
+  $('.navbar-links li').mouseover(function() {
+    $('.dropdown-list', this).stop().slideDown(400, 'linear')
+  })
+
+  $('.navbar-links li').mouseout(function() {
+    $('.dropdown-list', this).stop().slideUp(400, 'linear')
+  })
+
   // Toggle of Mobile sidebar
   $(".mobile-menu").click(function() {
     $("nav.sidebar").css({
@@ -18,6 +28,8 @@ $(document).ready(function() {
     });
   });
 
+
+  // Truncating Blog Text
   $(function() {
     $('.truncate').succinct({
       size: 100
@@ -27,8 +39,9 @@ $(document).ready(function() {
     });
   });
 
-  $(window).resize(function() {
-    $('.backgrounds')[0].slick.refresh();
+  // Load More Button
+  $('#myBtn').click(function() {
+    $('#more').slideToggle();
   });
 
   // Slick Carousel for Landing Page
@@ -37,9 +50,18 @@ $(document).ready(function() {
     cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: false,
+    arrows: true,
     dots: false,
     pauseOnDotsHover: false,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fas fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fas fa-angle-right' aria-hidden='true'></i></button>",
+    appendArrows: $(".button-container"),
+    responsive: [{
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+      }
+    }]
   });
 
   // Slick Carousel for Partners
@@ -47,6 +69,7 @@ $(document).ready(function() {
     autoplay: true,
     autoplaySpeed: 3000,
     dots: true,
+    appendDots: $(".dots-container"),
     infinite: true,
     speed: 300,
     slidesToShow: 1,
